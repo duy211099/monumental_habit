cfd86a3e53fdf0c2a192aa90adf1e57caa201cad
duy <duy.phambaxuan@haravan.com>
2022-03-31 22:32:48 +0700

    init

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kcSunsetColor = Color(0xfff65b4e);
+const Color kcTwilightColor = Color(0xff29319f);
+const Color kcMorningColor = Color(0xffffba7c);
+const Color kcFogColor = Color(0xffffdeef);
+const Color kcEclipseColor = Color(0xff573353);
